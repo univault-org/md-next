58540dc0813eb909ba22887e8c6f2f406f929bd8
Phil.T <amitacompany@yahoo.com>
2024-11-08 19:28:01 -0700

    fix: copy assets to out build folder

--- next.config.js
+++ next.config.js
@@ -2,8 +2,11 @@
 const nextConfig = {
   output: 'export',
   basePath: process.env.NODE_ENV === 'production' ? '/md-next' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/md-next/' : '',
   images: {
     unoptimized: true,
+    loader: 'custom',
+    path: '/md-next',
   },
   trailingSlash: true,
 }
